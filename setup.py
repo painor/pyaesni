cdae4a7a90cd9a4aa94274162e91267caf33f308
painor <pi.oussama@gmail.com>
2021-03-30 20:00:28 +0100

    case sensitive

--- setup.py
+++ setup.py
@@ -49,10 +49,10 @@ class CMakeBuild(build_ext):
                                    '-B', cmake_build_dir,
                                    '-DCMAKE_BUILD_TYPE={}'.format(cfg),
                                    *ext.cmake_options,
-                                   '-DPYTHON_EXECUTABLE={}'.format(sys.executable),
+                                   '-DPython_EXECUTABLE={}'.format(sys.executable),
                                    '-DPython_ROOT_DIR={}'.format(format(sys.executable)),
-                                   '-DPYTHON_INCLUDE_DIR={}'.format(get_python_inc()),
-                                   '-DPYTHON_LIBRARY={}'.format(sysconfig.get_config_var('LIBDIR')),
+                                   '-DPython_INCLUDE_DIR={}'.format(get_python_inc()),
+                                   '-DPython_LIBRARY={}'.format(sysconfig.get_config_var('LIBDIR')),
                                    '-DCMAKE_INSTALL_PREFIX={}'.format(install_dir),
                                    '-DPYTHON_VERSION=' + '.'.join(str(i) for i in sys.version_info[0:2])
                                    ],
