cc2e54210260d92f75c235be89d4deb487946568
painor <pi.oussama@gmail.com>
2021-03-30 20:18:56 +0100

    only version

--- setup.py
+++ setup.py
@@ -49,8 +49,6 @@ class CMakeBuild(build_ext):
                                    '-B', cmake_build_dir,
                                    '-DCMAKE_BUILD_TYPE={}'.format(cfg),
                                    *ext.cmake_options,
-                                   '-DPython_INCLUDE_DIR={}'.format(get_python_inc()),
-                                   '-DPython_LIBRARY={}'.format(sysconfig.get_config_var('LIBDIR')),                                   '-DCMAKE_INSTALL_PREFIX={}'.format(install_dir),
                                    '-DPYTHON_VERSION=' + '.'.join(str(i) for i in sys.version_info[0:3])
                                    ],
 
