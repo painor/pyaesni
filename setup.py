830d8009d335ef7a8b3f99c9981e97d1ba84bc99
painor <pi.oussama@gmail.com>
2021-04-02 23:00:39 +0100

    nvm drop 3.5 support

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name="pyaesni",
-    version="0.15",
+    version="0.16",
     ext_modules=[
         CMakeExtension(cmake_target="pyaesni",
                        cmake_lists_dir=".",
